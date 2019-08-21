73455719d7d1b95c97a773dc4643c17f5f213412
phonratichai <phonratichai11@gmail.com>
2019-08-22 00:26:14 +0700

    cal_bmi  and can change image

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:work_around/screen/authentication_module/login_design_screen.dart';
 import 'package:work_around/screen/running_history_module/running_history.dart';
@@ -10,12 +11,68 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomeScreen> {
+  //database reference
+  FirebaseDatabase database = new FirebaseDatabase();
+  FirebaseDatabase database2 = new FirebaseDatabase();
+  var width;
+  var height;
+  var _bmi;
+  String image_path;
+  String detail;
+  var profile =
+      FirebaseDatabase.instance.reference().child("profile").child("finfin");
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    database
+        .reference()
+        .child('profile')
+        .child("finfin")
+        .child("width")
+        .once()
+        .then((DataSnapshot snapshot) {
+      print('Connected to second database and read ${snapshot.value}');
+      setState(() {
+        width = "${snapshot.value}";
+      });
+    });
+    database2
+        .reference()
+        .child('profile')
+        .child("finfin")
+        .child("height")
+        .once()
+        .then((DataSnapshot snapshot) {
+      print('Connected to second database and read ${snapshot.value}');
+      setState(() {
+        height = "${snapshot.value}";
+      });
+    });
+  }
+
+  void set_bmi() {
+    setState(() {
+      var _height = int.parse(height);
+      var _width = int.parse(width);
+      _bmi = _width / ((_height / 100) * (_height / 100));
+      if (_bmi > 22.90) {
+        image_path = "assets/images/fat.jpg";
+        detail = "เริ่มอ้วน";
+      } else {
+        image_path = "assets/images/running.jpg";
+        detail = "สุขภาพดี";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    set_bmi();
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('หน้าหลัก'),
-        ),
+      ),
       drawer: new Drawer(
         child: new ListView(
           children: <Widget>[
@@ -33,27 +90,31 @@ class _HomePageState extends State<HomeScreen> {
               trailing: new Icon(Icons.home),
             ),
             new ListTile(
-              title:new Text("เริ่มวิ่งกันเลย"),
-              trailing: new Icon(Icons.directions_run),
-              onTap:() { 
-                Navigator.of(context).pop();
-                Navigator.push(context,MaterialPageRoute(builder:(context) => RunningPage()));
-                }
-            ),
-             new ListTile(
-              title:new Text("ตั้งเวลาการวิ่ง"),
-              trailing: new Icon(Icons.access_alarm),
-              onTap:() { 
-                Navigator.of(context).pop();
-                Navigator.push(context,MaterialPageRoute(builder:(context) => ListNotificationScreen(null,null)));
-                }
-            ),
+                title: new Text("เริ่มวิ่งกันเลย"),
+                trailing: new Icon(Icons.directions_run),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => RunningPage()));
+                }),
             new ListTile(
-              title:new Text("ประวัติการวิ่ง"),
+                title: new Text("ตั้งเวลาการวิ่ง"),
+                trailing: new Icon(Icons.access_alarm),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) =>
+                              ListNotificationScreen(null, null)));
+                }),
+            new ListTile(
+              title: new Text("ประวัติการวิ่ง"),
               trailing: new Icon(Icons.history),
-              onTap: (){
+              onTap: () {
                 Navigator.of(context).pop();
-                Navigator.push(context,MaterialPageRoute(builder: (context) => RunningHistory()));
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => RunningHistory()));
               },
             ),
             new Divider(),
@@ -62,39 +123,36 @@ class _HomePageState extends State<HomeScreen> {
               trailing: new Icon(Icons.settings),
             ),
             new ListTile(
-              title: new Text("ปิด"),
-              trailing: new Icon(Icons.exit_to_app),
-              onTap: () {
-                Navigator.of(context).pop();
-                Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
-              }
-            )
+                title: new Text("ปิด"),
+                trailing: new Icon(Icons.exit_to_app),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginScreen()));
+                })
           ],
         ),
       ),
-      body: 
-        Center(
-          child: Container(
-            child:
-              Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text("ส่วนสูง:xxx"),
-                    Text("น้ำหนัก:xxx"),
-                    Card(
-                      shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(15.0)),
-                      elevation: 3.0,
-                      child: Column(
-                        children: <Widget>[
-                          Text("knowleadge"),
-                          Image.asset("assets/images/running.jpg")
-                        ],
-                      ),
-                    ),
-                  ],
+      body: Center(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("ส่วนสูง: ${height}"),
+              Text("น้ำหนัก:${width}"),
+              Text("ค่าดัชนีมลวกาย (BMI):${_bmi}"),
+              Card(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15.0)),
+                elevation: 3.0,
+                child: Column(
+                  children: <Widget>[Text(detail), Image.asset(image_path)],
+                ),
               ),
-      ),
+            ],
+          ),
         ),
+      ),
     );
   }
 
